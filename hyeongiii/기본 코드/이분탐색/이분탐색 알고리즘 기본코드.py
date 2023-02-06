233e6eb38d79bca2648e09fcd71696907084d4db
hyeongiii <hyeongiii0811@gmail.com>
2023-02-07 08:35:03 +0900

    개념 : 이분 탐색 기본 코드 [이분 탐색]

--- hyeongiii/기본 코드/이분탐색/이분탐색 알고리즘 기본코드.py
+++ hyeongiii/기본 코드/이분탐색/이분탐색 알고리즘 기본코드.py
@@ -1,6 +1,6 @@
 # 비재귀적 이분 탐색의 파이썬 코드 (반복문)
 def binary_search(arr, val):
-    start, end = 0, arr.len()
+    start, end = 0, arr.len() - 1
     while start <= end:
         mid = (start + end) // 2
         if arr[mid] == val:
@@ -10,18 +10,20 @@ def binary_search(arr, val):
         else:
             start = mid + 1
 
-    return -1
+    return None
 
 
-# arr은 오름차순으로 정렬된 리스트
-def binary_search(arr, target, low=None, high=None):
-    low, high = low or 0, high or len(arr) - 1
+# 재귀 함수로 구현한 이진 탐색 소스 코드 (arr은 오름차순으로 정렬된 리스트)
+def binary_search(arr, target, low, high):
     if low > high:
-        return -1
+        return None
     mid = (low + high) // 2
-    if arr[mid] > target:
-        return binary_search(arr, target, low, mid)
+    # 찾은 경우 중간점 인덱스 반환
     if arr[mid] == target:
         return mid
-    if arr[mid] < target:
+    # 중간점의 값보다 찾고자 하는 값이 작은 경우, 왼쪽 확인
+    elif arr[mid] > target:
+        return binary_search(arr, target, low, mid - 1)
+    # 중간점의 값보다 찾고자 하는 값이 큰 경우, 오른쪽 확인
+    else:
         return binary_search(arr, target, mid + 1, high)
