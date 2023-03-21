47007a2ff18e504778ac2bd678ad71669eaa47b8
hyeongiii <hyeongiii0811@gmail.com>
2023-03-21 22:49:16 +0900

    BOJ : 14888 연산자 끼워넣기 [완전탐색]

---
+++ hyeongiii/JAVA/BOG/완전탐색/14888_연산자 끼워넣기.java
@@ -0,0 +1,103 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static StringBuilder sb = new StringBuilder();
+    static FastReader scan = new FastReader();
+
+    static int N, min, max;
+    static int[] nums, operators;
+
+    static void input() {
+        N = scan.nextInt();
+        nums = new int[N + 1];
+        operators = new int[5];
+
+        for (int i = 1; i <= N; i++) nums[i] = scan.nextInt();
+        for (int i = 1; i <= 4; i++) operators[i] = scan.nextInt();
+
+        min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
+    }
+
+    static int calculate(int operand1, int operator, int operand2) {
+        if (operator == 1) {
+            return operand1 + operand2;
+        } else if (operator == 2) {
+            return operand1 - operand2;
+        } else if (operator == 3) {
+            return operand1 * operand2;
+        } else {
+            return operand1 / operand2;
+        }
+    }
+
+    static void rec_func(int k, int value) {
+        if (k == N) {
+            max = Math.max(max, value);
+            min = Math.min(min, value);
+        } else {
+            for (int cand = 1; cand <= 4; cand++) {
+                if (operators[cand] >= 1) {
+                    operators[cand]--;
+                    rec_func(k + 1, calculate(value, cand, nums[k + 1]));
+                    operators[cand]++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        rec_func(1, nums[1]);
+        sb.append(max).append('\n').append(min);
+
+        System.out.println(sb.toString());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        public FastReader(String s) throws FileNotFoundException {
+            br = new BufferedReader(new FileReader(new File(s)));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
